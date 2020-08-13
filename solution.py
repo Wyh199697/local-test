9de354736df8aac7c2d6113b36cd5ee0ec265877
wyh199697 <708949379@qq.com>
2020-08-13 16:47:29 +0800

    +

--- solution.py
+++ solution.py
@@ -16,7 +16,7 @@ class Point:
         try:
             if ap.x == self.x:
                 return float("inf")
-            return math.tan((ap.y - self.y) / (ap.x - self.x))
+            return math.atan2((ap.y - self.y), (ap.x - self.x))
         except ZeroDivisionError:
             print(1)
 
@@ -25,48 +25,44 @@ class PointInfo:
     def __init__(self, num, points):
         self.num = num
         self.points = points
-        self.dis_info = {}
-        self.tan_info = {}
+        self.points_info = {}
         for i in range(num):
-            a, b = self.__add_info(self.points[i])
-            self.dis_info[self.points[i]] = a
-            self.tan_info[self.points[i]] = b
+            a = self.__add_info(self.points[i])
+            self.points_info[self.points[i]] = a
 
     def __add_info(self, p):
-        dis_info = {}
-        tan_info = {}
+        points_info = {}
         for i in range(self.num):
             if p != self.points[i]:
-                dis_info[self.points[i]] = p.cal_dis_2(self.points[i])
-                tan_info[self.points[i]] = p.cal_tan_angle(self.points[i])
-        return dis_info, tan_info
+                points_info[self.points[i]] = [math.sqrt(p.cal_dis_2(self.points[i])), p.cal_tan_angle(self.points[i])]
+        return points_info
 
     def show_info(self):
         for i in range(num):
-            dis = self.dis_info[final_points[i]]
-            tan = self.tan_info[final_points[i]]
+            info = self.points_info[final_points[i]]
             print("(%d, %d)" % (final_points[i].x, final_points[i].y))
-            for a in dis.keys():
-                print("(%d, %d)" % (a.x, a.y), dis[a], end=' ')
-                # print(b, tan[b])
-            print()
-            for b in tan.keys():
-                print("(%d, %d)" % (b.x, b.y), tan[b], end=' ')
+            for a in info.keys():
+                print("[(%d, %d)" % (a.x, a.y), info[a][0], '%.2f' % (info[a][1]) + ']', end=' ')
                 # print(b, tan[b])
             print()
 
-    def show_info(self, p):
-        dis = self.dis_info[final_points[i]]
-        tan = self.tan_info[final_points[i]]
-        print("(%d, %d)" % (final_points[i].x, final_points[i].y))
-        for a in dis.keys():
-            print("[(%d, %d)" % (a.x, a.y), str(dis[a]) + ']', end=' ')
+    '''def show_info(self, p):
+        info = self.points_info[p]
+        print("(%d, %d)" % (p.x, p.y))
+        for a in info.keys():
+            print("[(%d, %d)" % (a.x, a.y), info[a][0], str(info[a][1]) + ']', end=' ')
             # print(b, tan[b])
-        print()
-        for b in tan.keys():
-            print("[(%d, %d)" % (b.x, b.y), "%.2f" % tan[b] + ']', end=' ')
-            # print(b, tan[b])
-        print()
+        print()'''
+
+
+def have_nei(p, p_i, tan, dis, err_t, err_d):
+    flag = False
+    info = p_i.points_info[p]
+    for a in info.keys():
+        if dis < a[0] + err_d < dis:
+            if tan < a[1] + err_t < tan:
+                flag = True
+    return flag
 
 
 e_r = 10
@@ -81,34 +77,48 @@ for i in range(num):
     plt.annotate("%d(%.1f, %.1f)" % (i, final_points[i].x, final_points[i].y),
                  xy=(final_points[i].x, final_points[i].y))
 
-p1 = final_points[5]
-p2 = final_points[6]
 '''x = np.linspace(p1.x, p2.x, 10)
 y = (p1.cal_tan_angle(p2)) * (x - p1.x) + p1.x
 plt.plot(x, y, 'o')'''
-#plt.plot([p1.x, p2.x], [p1.y, p2.y], color='b')
+# plt.plot([p1.x, p2.x], [p1.y, p2.y], color='b')
 # plt.scatter([p1.x, p1.y], [p2.x, p2.y], color='b')
 p_i = PointInfo(num, final_points)
 '''for i in range(num):
     p_i.show_info(final_points[i])'''
+#p_i.show_info()
 
 num_p = input('0-19: ')
 r_o = Point(final_points[int(num_p)].x, final_points[int(num_p)].y)
 angle = random.uniform(-math.pi, math.pi)
-r_o.x += e_r * math.sin(angle)
-r_o.y += e_r * math.cos(angle)
+rr = random.randrange(e_r)
+r_o.x += rr * math.sin(angle)
+r_o.y += rr * math.cos(angle)
 plt.plot(r_o.x, r_o.y, 'bo')
 plt.plot([final_points[int(num_p)].x, r_o.x], [final_points[int(num_p)].y, r_o.y], color='b')
 x = np.arange(r_o.x - e_r, r_o.x + e_r, 0.1)
 y = np.arange(r_o.y - e_r, r_o.y + e_r, 0.1)
 x, y = np.meshgrid(x, y)
-plt.contour(x, y, (x-r_o.x)**2 + (y-r_o.y)**2, [e_r**2])     #x**2 + y**2 = 9 的圆形
-plt.axis('scaled')
-plt.show()
-n = 0
+plt.contour(x, y, (x - r_o.x) ** 2 + (y - r_o.y) ** 2, [e_r ** 2])  # x**2 + y**2 = 9 的圆形
+
+sum = 0
 for i in range(num):
     p = final_points[i]
     if pow(p.x - r_o.x, 2) + pow(p.y - r_o.y, 2) <= pow(e_r, 2):
-        n += 1
-print(n - 1)
-print(p1.cal_tan_angle(p2))
+        sum += 1
+print(sum)
+info = p_i.points_info[final_points[int(num_p)]]
+j = 5
+while j >= 0:
+    n = input('0-19: ')
+    n_p = final_points[int(n)]
+    o_p = final_points[int(num_p)]
+    k = info[n_p]
+    aa = math.atan2(-o_p.y + n_p.y, -o_p.x + n_p.x)
+    x = r_o.x + k[0] * math.cos(k[1])
+    y = r_o.y + k[0] * math.sin(k[1])
+    plt.plot(x, y, 'bo')
+    plt.plot([x, n_p.x], [y, n_p.y], 'b')
+    j -= 1
+plt.axis('scaled')
+
+plt.show()
